a6a59335f23b74d9b9368d7cd132c4c98b0acf01
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2022-10-29 18:50:42 +0200

    Fixed player animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,18 +11,27 @@ var in_area := ""
 onready var animationTree := $AnimationTree
 onready var animationState = $AnimationTree.get("parameters/playback")
 
+
+func _ready():
+	animationTree.active = true
+
+
 func _physics_process(delta):
 	# get movement
 	var vector = Vector2.ZERO
 	vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	vector = vector.normalized()
 
 	# move player
 	if vector != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_positon", velocity)
-		animationTree.set("parameters/Run/blend_positon", velocity)
-		velocity = velocity.move_toward(vector.normalized() * MAX_SPEED, ACCELERATION * delta)
+		animationTree.set("parameters/Idle/blend_position", vector)
+		animationTree.set("parameters/Run/blend_position", vector)
+		print(animationTree.get("parameters/Idle/blend_position"))
+		animationState.travel("Run")
+		velocity = velocity.move_toward(vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
 
